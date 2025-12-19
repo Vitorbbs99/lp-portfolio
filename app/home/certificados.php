a5f7c1bc0a69c2f11aa8772341ee9d362b712d5f
Vitor Barbosa <vitorbbs99@gmail.com>
2025-12-19 14:42:07 -0300

    Feature colors

--- app/home/certificados.php
+++ app/home/certificados.php
@@ -58,6 +58,17 @@
         </div>
       </div>
 
+      <div class="skill-block">
+          <figure class="skill-icon">
+            <img src="<?=URL_APP?>assets/dist/img/server.png" alt="Ícone Back-end">
+          </figure>
+          <div>
+            <h3 class="skill-title">UX/UI</h3>
+            <p class="skill-description">Desenvolvimento de APIs escaláveis, seguras e de alta performance (Node.js/Python).</p>
+          </div>
+        </div>
+      </div>
+
     </div>
     <!-- Bloco -->
 
